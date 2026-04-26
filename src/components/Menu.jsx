d5da85a89cd851d398545d4232317dd3bff87d4e
Scooby202 <erickscooby2021@gmail.com>
2026-04-26 18:11:20 -0300

    feat: reestruturação do layout e adição de estilos para a página principal e componentes

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -4,22 +4,30 @@ function Menu () {
 
     return (
         
-        <div className="Todo">
-            <div className="PrimeiraColuna">
-                <h1>Biblía Sagrada</h1>
+    <div className="app-container">
+        <aside className="sidebar sidebar-left">
+        <h2>Leitura Sagrada</h2>
+            {/* Lista de livros aqui */}
+        </aside>
+      
+        <main className="bible-content">
+            <h1 className="chapter-title">Gênesis 1</h1>
+            <div className="verses-container">
+                <span className="verse">
+                    <span className="verse-number">1</span>
+                        No princípio, criou Deus os céus e a terra.
+                </span>
+                <span className="verse">
+                    <span className="verse-number">2</span>
+                        E a terra era sem forma e vazia...
+                </span>
             </div>
-            <div className="SegundaColuna">
-                <h1>Livro</h1>
-            </div>
-            <div className="TerceiraColuna">
-                <div>
-                    <h1>botão</h1>
-                </div>
-                <div>
-                    <h1>Vesiculo do Dia</h1>
-                </div>
-            </div>
-        </div>
+        </main>
+        <aside className="sidebar sidebar-right">
+        <h2>Leitura Sagrada</h2>
+            {/* Lista de livros aqui */}
+        </aside>
+    </div>
 
     )
 
