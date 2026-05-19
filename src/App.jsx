8e8c63fb14e731c64d32a076c8096c1ad6a793b4
Scooby202 <erickscooby2021@gmail.com>
2026-05-19 08:49:49 -0300

    feat: adição de useEffect e rotas para Criar Conta e Anotações; remoção do componente BarradeAções

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Principal from './pages/Principal/Principal'
 import Login from './pages/Conta/Login/Login'
-//import RecuperarPage from './pages/Conta/EsqueceuSenha/EsqueceuSenha'
+import EsqueceuSenha from './pages/Conta/EsqueceuSenha/EsqueceuSenha'
 import {BrowserRouter, Route, Routes} from "react-router-dom"
+import CriarConta from './pages/Conta/CriarConta/CriarConta'
+import Anotações from './pages/Anotações/Anotações'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Principal/>}></Route>
         <Route path='/Login.jsx' element={<Login/>}></Route>
+        <Route path='/EsqueceuSenha.jsx' element={<EsqueceuSenha/>}></Route>
+        <Route path='/CriarConta.jsx' element={<CriarConta/>}></Route>
+        <Route path='/Anotações.jsx' element={<Anotações/>}></Route>
       </Routes>
     </BrowserRouter>
 
--- src/components/BarradeAcões.jsx
+++
@@ -1,5 +0,0 @@
-function BarradeAcões () {
-
-}
-
-export default BarradeAcões
