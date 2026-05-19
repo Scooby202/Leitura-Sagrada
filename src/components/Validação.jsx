f6eab8f13dd78173d5ec9e342178e4d73a1a3634
Scooby202 <erickscooby2021@gmail.com>
2026-05-19 08:51:35 -0300

    feat: criação do componente Validação para verificar e-mail e senha

---
+++ src/components/Validação.jsx
@@ -0,0 +1,68 @@
+function Validação () {
+
+
+const form = document.querySelector("#form");
+const emailInput = document.querySelector("#email");
+const passwordInput = document.querySelector("#password");
+const ContinuaInfo = document.querySelector("#Continua");
+const ContinuaInfo2 = document.querySelector("#Continua2");
+
+    //Verificar o e-mail
+
+    if(emailInput) {
+        if(emailInput.value === "" || !VerificarEmail(emailInput.value)) {
+            alert("Por favor, insira o seu E-mail")
+            return false;
+        };
+
+    };
+
+    //Verificar a senha
+    if(passwordInput) {
+        if(!VerificarSenha(passwordInput.value, 8)) {
+            alert("Senha tem menos de 8 digitos")
+            return false;
+        };
+
+        // tem letra
+
+        const temLetra = /[a-zA-Z]/.test(passwordInput.value);
+
+        if (!temLetra) {
+            alert("Senha precisa de pelo menos uma letra.")
+            return false;
+        }
+    };
+
+    //Se todos estiverem preenchindo
+    
+
+        form.requestSubmit();
+        return;
+
+        function VerificarEmail (email) {
+            //regex para validar
+            const emailRegex = new RegExp(
+                // exemplo@gmail.com
+                /^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]{2,}$/
+
+            );
+
+            if(emailRegex.test(email)) {
+                return true
+            };
+            return false;
+        };
+    
+        function VerificarSenha(password, minDigitos) {
+            if(password.length >= minDigitos) {
+                return true;
+            };
+            
+            return false;
+            
+        };
+
+}
+
+export default Validação
