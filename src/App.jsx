3a61f739a3b2285d9acdb6a1822e79ec6a71e6de
Scooby202 <erickscooby2021@gmail.com>
2026-05-22 23:04:21 -0300

    feat: adição das rotas para as páginas de Código de Esqueceu Senha e Nova Senha

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import EsqueceuSenha from './pages/Conta/EsqueceuSenha/EsqueceuSenha'
 import {BrowserRouter, Route, Routes} from "react-router-dom"
 import CriarConta from './pages/Conta/CriarConta/CriarConta'
 import Anotações from './pages/Anotações/Anotações'
+import CodigoEsqueceuSenha from './pages/Conta/EsqueceuSenha/CodigoEsqueceuSenha'
+import NovaSenha from './pages/Conta/EsqueceuSenha/NovaSenha'
 
 function App() {
   const [count, setCount] = useState(0);
@@ -17,6 +19,8 @@ function App() {
         <Route path='/EsqueceuSenha.jsx' element={<EsqueceuSenha/>}></Route>
         <Route path='/CriarConta.jsx' element={<CriarConta/>}></Route>
         <Route path='/Anotações.jsx' element={<Anotações/>}></Route>
+        <Route path='/CodigoEsqueceuSenha.jsx' element={<CodigoEsqueceuSenha/>}></Route>
+        <Route path='/NovaSenha.jsx' element={<NovaSenha/>}></Route>
       </Routes>
     </BrowserRouter>
 
